b6cea240aceabd2cb8b25a835ff97f90761b7fd1
Emmadegreat <mkpurunchiemma@gmail.com>
2022-08-18 08:17:26 -0700

    added

--- blog.js
+++ blog.js
@@ -1,5 +1,9 @@
-const p = document.querySelector('.btn');
-p.addEventListener('click', (e) => {
-    e.preventDefault();
-    document.querySelector('p').style.backgroundColor = 'yellow';
-})
+const btn = document.querySelectorAll('.blog');
+btn.forEach(blogz => {
+    const more = blogz.querySelector('.read-more');
+    const pp = blogz.querySelector('p');
+    more.addEventListener('click', (e)=>{
+        e.preventDefault();
+        pp.style.backgroundColor = 'navy';
+    })
+});

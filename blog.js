4e3634d910c66bf405ad00b010b973c2a69916f0
Emmadegreat <mkpurunchiemma@gmail.com>
2022-08-18 18:06:20 -0700

    committed

--- blog.js
+++ blog.js
@@ -1,9 +1,29 @@
-const btn = document.querySelectorAll('.blog');
-btn.forEach(blogz => {
-    const more = blogz.querySelector('.read-more');
-    const pp = blogz.querySelector('p');
-    more.addEventListener('click', (e)=>{
-        e.preventDefault();
-        pp.style.backgroundColor = 'navy';
-    })
-});
+TweenLite.defaultEase = Expo.easeOut;
+const Blog = () =>{
+
+    const blogs = document.querySelectorAll('.blog');
+    blogs.forEach(blog => {
+        const btn = blog.querySelector('.read-more');
+        const pa = blog.querySelector('p');
+        
+        btn.addEventListener('click', (e)=>{
+            e.preventDefault();
+            if (btn.textContent === 'read more') {
+                btn.textContent = 'intro...';
+            } else {
+                btn.textContent = 'read more'
+            }
+            
+            const h = getComputedStyle(pa).getPropertyValue('max-Height');
+            if (h == '80px') {
+                TweenMax.fromTo(pa, 0.2, {maxHeight: '80px', y: 100 }, {maxHeight: '100%', y:0});
+            }
+            if (h == '100%') {
+                TweenMax.fromTo(pa, 0.2, {maxHeight: '100%', y: -100 }, {maxHeight: '80px', y:0});
+            }
+
+            
+        });
+    });
+}
+Blog();
